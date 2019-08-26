4a8ebefeb910303bc170e20cfb2eee118fede8f1
takehir00 <0kutakehiro15@gmail.com>
2019-08-27 08:44:07 +0900

    createPlayFrameworkDir

---
+++ src/main/java/com/nanpa/nanpa_picks/NanpaPicksApplication.java
@@ -0,0 +1,13 @@
+package com.nanpa.nanpa_picks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanpaPicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanpaPicksApplication.class, args);
+	}
+
+}
