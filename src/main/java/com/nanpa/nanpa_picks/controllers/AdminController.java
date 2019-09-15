678feefb6b26030b6bbcde696c43cb677dc41005
takehir00 <0kutakehiro15@gmail.com>
2019-09-15 21:09:42 +0900

    管理画面top

---
+++ src/main/java/com/nanpa/nanpa_picks/controllers/AdminController.java
@@ -0,0 +1,22 @@
+package com.nanpa.nanpa_picks.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+    /**
+     * 管理画面TOP遷移
+     *
+     * @param mav
+     * @return
+     */
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public ModelAndView top(ModelAndView mav) {
+        mav.setViewName("admin/top");
+        return mav;
+    }
+}
