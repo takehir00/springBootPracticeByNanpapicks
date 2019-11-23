80310a893dd67946922d28a5e4dfba1713b387bb
takehir00 <0kutakehiro15@gmail.com>
2019-11-23 16:10:28 +0900

    不要なimport削除

--- client/src/main/java/client/security/BaseSecurityConfig.java
+++ client/src/main/java/client/security/BaseSecurityConfig.java
@@ -8,12 +8,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import javax.activation.DataSource;
 
 @Configuration
 @EnableWebSecurity
