a561e9c8cefcc1ddeff1183135f77b7b843cfe49
takehir00 <0kutakehiro15@gmail.com>
2019-12-04 08:53:56 +0900

    コメントサービスクラス

---
+++ admin/src/main/java/admin/services/CommentService.java
@@ -0,0 +1,4 @@
+package admin.services;
+
+public interface CommentService {
+}
---
+++ admin/src/main/java/admin/services/impl/CommentServiceImpl.java
@@ -0,0 +1,7 @@
+package admin.services.impl;
+
+import admin.services.CommentService;
+
+
+public class CommentServiceImpl implements CommentService {
+}
