fc0caf6362f8a0074b904ae291391c981e1c02ce
takehir00 <0kutakehiro15@gmail.com>
2019-12-12 08:32:16 +0900

    コメント登録フォームクラス

--- admin/src/main/java/admin/forms/comment/CommentRegisterForm.java
+++ admin/src/main/java/admin/forms/comment/CommentRegisterForm.java
@@ -1,12 +1,33 @@
 package admin.forms.comment;
 
+import db.entities.Article;
+import db.entities.Comment;
+import db.entities.User;
 import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Tolerate;
 
 import java.util.List;
 
 @Builder
+@Data
 public class CommentRegisterForm {
-    public List<Long> userIdList;
+    @Tolerate
+    public CommentRegisterForm(){};
 
-    public List<Long> articleIdList;
+    public String content;
+    public Long userId;
+    public Long articleId;
+
+    public Comment asEntity() {
+        User user = new User();
+        user.id = userId;
+        Article article = new Article();
+        article.id =articleId;
+        return Comment.builder()
+                .content(this.content)
+                .user(user)
+                .article(article)
+                .build();
+    }
 }
