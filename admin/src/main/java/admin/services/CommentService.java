75c395a8a30c318bd96a516b15f7d5b3fa0e1064
takehir00 <0kutakehiro15@gmail.com>
2019-12-21 13:03:20 +0900

    コメント更新フォーム生成

--- admin/src/main/java/admin/services/CommentService.java
+++ admin/src/main/java/admin/services/CommentService.java
@@ -3,6 +3,7 @@ package admin.services;
 import admin.forms.comment.CommentRegisterForm;
 import admin.responses.CommentRegisterFormResponse;
 import admin.responses.CommentTopResponse;
+import admin.responses.CommentUpdateFormResponse;
 
 public interface CommentService {
     /**
@@ -25,4 +26,12 @@ public interface CommentService {
      * @param commentRegisterForm
      */
     void create(CommentRegisterForm commentRegisterForm);
+
+    /**
+     * 更新フォーム
+     *
+     * @param commentId
+     * @return
+     */
+    CommentUpdateFormResponse updateForm(Long commentId);
 }
