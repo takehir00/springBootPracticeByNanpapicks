79b14b3f5b94657f6b50b7fb388cd93a518b840b
takehir00 <0kutakehiro15@gmail.com>
2019-12-30 12:02:47 +0900

    記事登録フォームクラス

---
+++ admin/src/main/java/admin/forms/article/ArticleRegisterForm.java
@@ -0,0 +1,25 @@
+package admin.forms.article;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.Tolerate;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Builder
+@Data
+public class ArticleRegisterForm {
+    @Tolerate
+    public ArticleRegisterForm(){};
+
+    @NotEmpty
+    public String url;
+
+    @NotEmpty
+    @Size(max = 100, message = "100文字以下で入力してください")
+    public String title;
+
+    @NotEmpty
+    public String imageUrl;
+}
