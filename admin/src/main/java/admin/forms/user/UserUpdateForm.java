c25e431c731de0227d5d2e214f97d80127ca2892
takehir00 <0kutakehiro15@gmail.com>
2019-12-30 09:58:12 +0900

    ユーザー更新フォームクラス

---
+++ admin/src/main/java/admin/forms/user/UserUpdateForm.java
@@ -0,0 +1,27 @@
+package admin.forms.user;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Builder
+@Data
+public class UserUpdateForm {
+    public Long id;
+
+    @NotEmpty
+    @Size(max = 30, message = "30文字以下で入力してください")
+    public String name;
+
+    @NotEmpty
+    @Email
+    public String mail;
+
+    @Size(max = 500, message = "500文字以下で入力してください")
+    public String introduction;
+
+    public String imageUrl;
+}
