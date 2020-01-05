b85c6e92846e523fc7ce00135cc2ec1bafb1e06c
takehir00 <0kutakehiro15@gmail.com>
2020-01-06 08:10:58 +0900

    記事トップ画面用レスポンスクラスにページ数フィールドを追加

--- client/src/main/java/client/responses/articles/ArticleIndexResponse.java
+++ client/src/main/java/client/responses/articles/ArticleIndexResponse.java
@@ -1,9 +1,14 @@
 package client.responses.articles;
 
 import db.entities.Article;
+import lombok.Builder;
+import lombok.Data;
 
 import java.util.List;
 
+@Builder
+@Data
 public class ArticleIndexResponse {
     List<Article> articleList;
+    int pageCount;
 }
