02ca67f0ea77089cac52ae826b1dd98a8eedb328
takehir00 <0kutakehiro15@gmail.com>
2020-01-06 08:13:15 +0900

    articleRepositoryの実装をjpaRepositoryを使った実装からjpa,jpqlを使った実装に変更、offset,limitを使ったfindメソッドを追加

--- db/src/main/java/db/repositories/ArticleRepository.java
+++ db/src/main/java/db/repositories/ArticleRepository.java
@@ -2,12 +2,56 @@ package db.repositories;
 
 
 import db.entities.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
-@Repository
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Optional<Article> findById(Long id);
+@Component
+public class ArticleRepository {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Article findById(Long articleId) {
+        return entityManager.createQuery(
+                "select article from Article article where article.id = :articleId", Article.class)
+                .setParameter("articleId", articleId)
+                .getSingleResult();
+    }
+
+    public List<Article> findAll() {
+        return entityManager.createQuery(
+                "select article from Article article", Article.class)
+                .getResultList();
+    }
+
+    public int countAll() {
+        List<Article> articleList = entityManager.createQuery(
+                "select article from Article article", Article.class)
+                .getResultList();
+        return articleList.size();
+    }
+
+    public List<Article> findByOffsetAndLimit(int offset, int limit) {
+        TypedQuery<Article> query = entityManager
+                .createQuery(
+                        "select article from Article article ORDER BY article.id DESC", Article.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit);
+        return query.getResultList();
+    }
+
+    public void create(Article entity) {
+        entityManager.persist(entity);
+    }
+
+    public void update(Article entity) {
+        entityManager.merge(entity);
+    }
+
+    public void delete(Article entity) {
+        entityManager.remove(entity);
+    }
 }
