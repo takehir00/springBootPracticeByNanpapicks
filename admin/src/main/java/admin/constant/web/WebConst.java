4962e5443e8881eec77366fbde4dbff6e6ce9035
takehir00 <0kutakehiro15@gmail.com>
2020-02-24 11:32:11 +0900

    インターセプターでログ制御してみる、うまく機能しない

---
+++ admin/src/main/java/admin/constant/web/WebConst.java
@@ -0,0 +1,8 @@
+package admin.constant.web;
+
+public class WebConst {
+    /** ---- MDC ---- **/
+    public static final String MDC_LOGIN_USER_ID = "LOGIN_USER_ID";
+
+    public static final String MDC_FUNCTION_NAME = "FUNCTION_NAME";
+}
--- admin/src/main/java/admin/controllers/ArticleController.java
+++ admin/src/main/java/admin/controllers/ArticleController.java
@@ -1,12 +1,15 @@
 package admin.controllers;
 
 
+import admin.common.FunctionNameAware;
 import admin.forms.article.ArticleForm;
 import admin.forms.article.ArticleRegisterForm;
 import admin.forms.article.ArticleUpdateForm;
 import admin.util.PageUtil;
 import db.entities.Article;
 import admin.services.ArticleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
@@ -19,7 +22,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.transaction.Transactional;
 
 @Controller
-public class ArticleController {
+public class ArticleController implements FunctionNameAware {
 
     @Autowired
     ArticleService articleService;
@@ -33,6 +36,8 @@ public class ArticleController {
     @Transactional
     @RequestMapping(value = "/admin/article", method = RequestMethod.GET)
     public ModelAndView top(ModelAndView mav, @RequestParam int page) {
+        Logger logger = LoggerFactory.getLogger("記事一覧画面にアクセス");
+        logger.debug("記事一覧画面にアクセス");
         mav = setArticleTopModelAndView(null, page);
         return mav;
     }
@@ -175,5 +180,10 @@ public class ArticleController {
         mav.addObject("pageCount", articleService.getPageCount(limit));
         mav.addObject("flash",flash);
         return mav;
+    }
+
+    @Override
+    public String getFunctionName() {
+        return "記事コントローラーーーー！！";
     }
 }
