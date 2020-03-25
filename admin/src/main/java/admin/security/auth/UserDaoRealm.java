15132795c3fbf1b980ef6d20432bb591a09afb69
takehir00 <0kutakehiro15@gmail.com>
2020-03-25 11:09:08 +0900

    管理画面のログインログアウト機能

---
+++ admin/src/main/java/admin/security/auth/UserDaoRealm.java
@@ -0,0 +1,37 @@
+package admin.security.auth;
+
+import db.daos.impl.UserDaoImpl;
+import db.entities.User;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.ArrayList;
+import java.util.List;
+
+@EntityScan(basePackageClasses = User.class)
+@EnableJpaRepositories(basePackageClasses = UserDaoImpl.class)
+@Component
+public class UserDaoRealm implements UserDetailsService {
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("USER"));
+        //repositoryにmailで取得するメソッド定義する
+        UserDaoImpl userDao = new UserDaoImpl(entityManager);
+        User entity = userDao.findByMail(mail)
+                .orElseThrow(() -> new UsernameNotFoundException(""));
+
+        return new LoginUser(entity.mail, entity.password, authorityList);
+    }
+}
